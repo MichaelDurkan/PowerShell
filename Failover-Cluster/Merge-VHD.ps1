e6e0f36f435f96d65098d37e4b9a0fb37e092683
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-04-26 12:38:53 +0100

    Update Merge-VHD.ps1

--- Failover-Cluster/Merge-VHD.ps1
+++ Failover-Cluster/Merge-VHD.ps1
@@ -5,3 +5,6 @@ $Merge = @{
   DestinationPath = 'c:\artofshell\client01.vhdx'
 }
 Merge-VHD @Merge
+
+# If you get an error from the above command, you need to install the PS Module for Hyper-V
+# To do this, run ```Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell```
