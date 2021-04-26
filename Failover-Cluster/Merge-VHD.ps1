6a683ecfe861b335c585020710572d65c093bd5f
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-04-26 12:39:06 +0100

    Update Merge-VHD.ps1

--- Failover-Cluster/Merge-VHD.ps1
+++ Failover-Cluster/Merge-VHD.ps1
@@ -7,4 +7,4 @@ $Merge = @{
 Merge-VHD @Merge
 
 # If you get an error from the above command, you need to install the PS Module for Hyper-V
-# To do this, run ```Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell```
+# To do this, run "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell"
